90f35f6aa5e7a955b426f43d96007e967a025a2c
Ben Buttigieg <bebuttig@microsoft.com>
2018-11-22 11:10:30 +0000

    SignalR communicator app broadcast working on iOS and Droid

---
+++ samples/communicator/Communicator/Communicator/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Communicator
+{
+	public partial class MainPage : ContentPage
+	{
+
+        MainViewModel _viewmodel;
+		public MainPage()
+		{
+			InitializeComponent();
+            this.BindingContext = _viewmodel = new MainViewModel();
+        }
+
+        private void SendClicked(object sender, EventArgs e)
+        {
+            _viewmodel.SendMessage();
+        }
+
+        private async void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            await _viewmodel.ConnectToHub();
+        }
+    }
+}
---
+++ samples/communicator/Communicator/Communicator/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.MobCAT.MVVM;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Communicator
+{
+
+    public class MainViewModel : BaseNavigationViewModel
+
+    {
+        HubConnection _hubConnection;
+        public ObservableCollection<string> _messages;
+
+        public MainViewModel()
+        {
+            Messages = new ObservableCollection<string>();
+            _hubConnection = new HubConnectionBuilder()
+              .WithUrl($"https://mccommunicator.azurewebsites.net/chat")
+              .Build();
+            _hubConnection.Closed += _hubConnection_Closed;
+            _hubConnection.On<string, string>("broadcastMessage", (user, message) =>
+             {
+                 Messages.Add(message);
+             });
+        }
+
+        public ObservableCollection<string> Messages
+        {
+            get { return _messages; }
+            set
+            {
+                RaiseAndUpdate(ref _messages, value);
+            }
+        }
+
+        internal void SendMessage()
+        {
+            _hubConnection.InvokeAsync("BroadcastMessage",
+                "BenBtgTestApp",
+                "Testing testing."
+                );
+        }
+
+        private Task _hubConnection_Closed(Exception arg)
+        {
+            Messages.Add("Connection closed");
+            return ConnectToHub();
+        }
+
+        public async Task ConnectToHub()
+        {
+            Messages.Add("Opening connection...");
+
+            try
+            {
+                await _hubConnection.StartAsync();
+                Messages.Add("Connection open.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private async void ConnectClicked(object sender, EventArgs e)
+        {
+            await ConnectToHub();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _hubConnection.InvokeAsync("BroadcastMessage",
+                "BenBtgTestApp",
+                "Testing testing."
+                );
+        }
+    }
+}
