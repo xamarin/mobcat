e0658c8106db1ef9a3c105207093b8b52cf685e6
Mike Parker <miparker@microsoft.com>
2018-11-27 08:34:03 +0000

    Initial implementation for client secrets Fody weaver

---
+++ mobcat_client_secrets/src/AssemblyToProcess/SampleImplicitConfig.cs
@@ -0,0 +1,13 @@
+﻿using MobCAT.ClientSecrets;
+
+namespace AssemblyToProcess
+{
+    public class SampleImplicitConfig
+    {
+        [ClientSecret]
+        public const string MyConstantKeyName = SampleConfigTestValues.LOOKUP_KEY_B;
+
+        [ClientSecret]
+        public string MyKeyName = SampleConfigTestValues.LOOKUP_KEY_B;
+    }
+}
---
+++ mobcat_client_secrets/src/AssemblyToProcess/SampleStaticConfig.cs
@@ -0,0 +1,16 @@
+﻿using MobCAT.ClientSecrets;
+
+namespace AssemblyToProcess
+{
+    public static class SampleStaticConfig
+    {
+        [ClientSecret(SampleConfigTestValues.LOOKUP_KEY_A)]
+        public const string MyConstantKeyName = SampleConfigTestValues.LOOKUP_KEY_B;
+
+        [ClientSecret(SampleConfigTestValues.LOOKUP_KEY_A)]
+        public static string MyStaticKeyName = SampleConfigTestValues.LOOKUP_KEY_B;
+
+        [ClientSecret(SampleConfigTestValues.LOOKUP_KEY_A)]
+        public static readonly string MyStaticReadonlyKeyName = SampleConfigTestValues.LOOKUP_KEY_B;
+    }
+}
