251127498c21e1fea626a45338152712597a394b
Dean Faizal <defaizal@microsoft.com>
2018-12-03 12:04:54 -0500

    More debugging on devops-environment.sh

--- samples/weather/xamarin/build/devops-environment.sh
+++ samples/weather/xamarin/build/devops-environment.sh
@@ -49,21 +49,25 @@ echo "- WeatherServiceApiKey"
 ##vso[task.setvariable variable=WeatherServiceApiKey;;issecret=true] $API_KEY
 echo "Build_WeatherServiceApiKey: $BUILD_WEATHERSERVICEAPIKEY"
 echo "WeatherServiceApiKey: $WeatherServiceApiKey"
+echo "WEATHERSERVICEAPIKEY: $WEATHERSERVICEAPIKEY"
 
 echo "- WeatherServiceUrl"
 ##vso[task.setvariable variable=WeatherServiceUrl;;issecret=true] $SERVICE_ENDPOINT
 echo "Build_WeatherServiceUrl: $BUILD_WEATHERSERVICEURL"
-echo "WeatherServiceUrl: $WeatherServiceUrl"
+echo "WeatherServiceUrl: $WeatherServiceUrl""
+echo "WEATHERSERVICEURL: $WEATHERSERVICEURL"
 
 echo "- AndroidAppCenterSecret"
 ##vso[task.setvariable variable=AndroidAppCenterSecret;;issecret=true] $ANDROID_APP_CENTER_SECRET
 echo "Build_AndroidAppCenterSecret: $BUILD_ANDROIDAPPCENTERSECRET"
 echo "AndroidAppCenterSecret: $AndroidAppCenterSecret"
+echo "ANDROIDAPPCENTERSECRET: $ANDROIDAPPCENTERSECRET"
 
 echo "- iOSAppCenterSecret"
 ##vso[task.setvariable variable=iOSAppCenterSecret;;issecret=true] $iOS_APP_CENTER_SECRET
 echo "Build_iOSAppCenterSecret: $BUILD_IOSAPPCENTERSECRET"
 echo "iOSAppCenterSecret: $iOSAppCenterSecret"
+echo "IOSAPPCENTERSECRET: $IOSAPPCENTERSECRET"
 
 echo ""
 echo "========= WeatherSample Environment Configuration completed ========="
