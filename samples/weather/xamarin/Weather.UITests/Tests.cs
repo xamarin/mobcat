d63bd808b630c94a74bafaf61607587e2fe6fded
Dean Faizal <defaizal@microsoft.com>
2018-12-19 09:36:44 -0500

    Add delay for SetLocation in UITest
    Potential fix for SetLocation not working

--- samples/weather/xamarin/Weather.UITests/Tests.cs
+++ samples/weather/xamarin/Weather.UITests/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using NUnit.Framework;
 using Weather.UITests.Pages;
 using Xamarin.UITest;
@@ -18,6 +19,9 @@ namespace Weather.UITests
         public void SetLocationRedmond()
         {
             app.Device.SetLocation(latitude: 47.6739, longitude: -122.1215);
+
+            Thread.Sleep(TimeSpan.FromSeconds(15)); //Wait for the location to be set on the device
+
             new MainPage().SetLocationToRedmond();
         }
 
