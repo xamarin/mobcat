f58c60c7543c24d1267c4c87cd1bf63402ea2c4b
Ben Buttigieg <bebuttig@microsoft.com>
2019-02-10 22:23:03 +0000

    Created FountainView plugin

---
+++ scratch/flutter/FountainView/Flutter/fountainview/ios/Classes/FountainviewPlugin.m
@@ -0,0 +1,20 @@
+#import "FountainviewPlugin.h"
+
+@implementation FountainviewPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fountainview"
+            binaryMessenger:[registrar messenger]];
+  FountainviewPlugin* instance = [[FountainviewPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
