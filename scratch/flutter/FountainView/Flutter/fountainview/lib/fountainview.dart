99bb1963ef704a20280abe1414d7ab2fa08469ff
Ben Buttigieg <bebuttig@microsoft.com>
2019-02-10 22:17:52 +0000

    Basic widget from plugin working

--- scratch/flutter/FountainView/Flutter/fountainview/lib/fountainview.dart
+++ scratch/flutter/FountainView/Flutter/fountainview/lib/fountainview.dart
@@ -1,13 +1,36 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_html/flutter_html.dart';
 
-class Fountainview {
-  static const MethodChannel _channel =
-      const MethodChannel('fountainview');
+class FountainView extends StatelessWidget {
+  const FountainView(this.fountainText);
+  final String fountainText;
 
-  static Future<String> get platformVersion async {
+ @override
+  Widget build(BuildContext context) {
+    return new FutureBuilder(
+      future: platformVersion(fountainText),
+      initialData: "Loading text..",
+      builder: (BuildContext context, AsyncSnapshot<String> text) {
+        return new Html(data: text.data);
+      });
+  }
+
+  static const MethodChannel _channel = const MethodChannel('fountainview');
+
+  static const MethodChannel _newchannel = const MethodChannel('fountain.mobcat/fountainlib');
+
+  static Future<String> get convertFountainToHtml async {
+  
+     final String fountainDoc = await _channel.invokeMethod('ConvertToHtml', "This is a test");
+
+    return fountainDoc;
+  }
+
+  static Future<String> platformVersion(String text) async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+    return version + text;
   }
 }
--- scratch/flutter/FountainView/Flutter/fountainviewsample/lib/main.dart
+++ scratch/flutter/FountainView/Flutter/fountainviewsample/lib/main.dart
@@ -89,8 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: 
-            Html(data:'$_fountainText',
-            ),
+            Html(data:'$_fountainText'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _convertToHtml,
