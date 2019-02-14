d718f4ef6df83a839cf5aaddeeb4cde712199fff
Ben Buttigieg <bebuttig@microsoft.com>
2019-02-14 11:19:00 +0000

    Updated Droid wrapper lib and native sample

--- scratch/flutter/FountainView/Xamarin/FountainSharpWrapper/EmbeddinatorDroid.sh
+++ scratch/flutter/FountainView/Xamarin/FountainSharpWrapper/EmbeddinatorDroid.sh
@@ -5,4 +5,6 @@ echo Running Embeddinator Droid Build...
 export TOOLS=~/.nuget/packages/embeddinator-4000/0.4.0/tools
 export PATH=$PATH:/Library/Frameworks/Mono.framework/Commands
 
-mono $TOOLS/Embeddinator-4000.exe 'FountainLib.Droid/bin/Release/FountainLib.Droid.dll' -gen=Java -platform=Android --outdir='bin/android' -c
+mono $TOOLS/Embeddinator-4000.exe 'FountainSharpWrapperDroid/bin/Release/FountainSharpWrapperDroid.dll' -gen=Java -platform=Android --outdir='bin/android' -c
+cp bin/android/FountainSharpWrapperDroid.aar ../../Native/DroidSampleApp/app/libs
+cp bin/android/FountainSharpWrapperDroid.aar ../../Flutter/fountainview/android/libs
--- scratch/flutter/FountainView/Xamarin/FountainSharpWrapper/FountainSharpWrapperDroid/FountainSharpWrapper.cs
+++ scratch/flutter/FountainView/Xamarin/FountainSharpWrapper/FountainSharpWrapperDroid/FountainSharpWrapper.cs
@@ -3,10 +3,10 @@ using Java.Interop;
 
 namespace FountainSharpWrapperDroid
 {
-    [Register("mono.embeddinator.android.FountainSharpWrapper")]
+    
     public static class FountainSharpWrapper
     {
-        [Export("ConvertToHtml")]
+        
         public static string ConvertToHtml(string FountainText)
         {
             return FountainSharp.HtmlFormatter.TransformHtml(FountainText);
