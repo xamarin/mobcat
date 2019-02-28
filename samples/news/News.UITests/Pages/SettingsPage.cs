90574bc272925b3afa9977c4033fb21d04b99685
DeanFaizal <dean.faizal@microsoft.com>
2019-02-28 10:44:53 -0500

    Initial implementation of UITests

---
+++ samples/news/News.UITests/Pages/SettingsPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace News.UITests.Pages
+{
+    public class SettingsPage : BasePage
+    {
+        protected override PlatformQuery Trait => new PlatformQuery
+        {
+            Android = x => x.Marked(nameof(SettingsPage)),
+            iOS = x => x.Marked(nameof(SettingsPage))
+        };
+
+        protected readonly Query LightModeSwitch;
+
+        public SettingsPage()
+        {
+            LightModeSwitch = x => x.Marked(nameof(LightModeSwitch));
+        }
+
+        public void SetLightMode()
+        {
+            app.WaitForElement(LightModeSwitch);
+            app.Tap(LightModeSwitch);
+            app.Screenshot("Toggled light mode");
+        }
+    }
+}
