142227766493d4f4f00fc1308549718c6c4a6c6d
Sweeky Satpathy <swsat@microsoft.com>
2019-03-01 16:14:44 -0800

    main README scratch description updated, Client secret nuget removed and solution files updated

---
+++ samples/news/News/ServiceConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace News
+{
+    /// <summary>
+    /// Secret key file.
+    /// Replace these values with your API Key values
+    /// </summary>
+    public static class ServiceConfig
+    {
+        public static readonly string NEWSSERVICEAPIKEY = string.Empty;
+
+        public static readonly string NEWSSERVICEURL = string.Empty;
+
+        public static readonly string ANDROIDAPPCENTERSECRET = string.Empty;
+
+        public static readonly string IOSAPPCENTERSECRET = string.Empty;
+    }
+}
