b6f0cd169b48263fbf11add2c5160b71c0f60a59
Alexey Strakh <alexey.strakh@microsoft.com>
2019-04-02 14:18:03 -0700

    restore calabash link for release build to run tests in appcenter from the pipeline build

--- samples/news/News.iOS/AppDelegate.cs
+++ samples/news/News.iOS/AppDelegate.cs
@@ -8,11 +8,7 @@ namespace News.iOS
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-
-#if DEBUG
             Xamarin.Calabash.Start();
-#endif
-
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
             Lottie.Forms.iOS.Renderers.AnimationViewRenderer.Init();
