57775e79b503d6c3b3cabb52e261738ea3c90803
Mike Parker <mikeparker@mikes-mbp-2.home>
2019-06-14 09:47:11 +0100

    Updated cpp_libs_xamarin read me addressing Xamarin.iOS limitations

--- scratch/xamarin/cpp_libs_xamarin/src/SampleApp/SampleLib/SampleFuncsWrapper.cs
+++ scratch/xamarin/cpp_libs_xamarin/src/SampleApp/SampleLib/SampleFuncsWrapper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace SampleLib
 {
@@ -11,6 +14,7 @@ namespace SampleLib
         const string DllName = "__Internal";
 #endif
 
+        static ConcurrentDictionary<int, TaskCompletionSource<IEnumerable<int>>> fibonacciCallbacks = new ConcurrentDictionary<int, TaskCompletionSource<IEnumerable<int>>>();
         internal delegate void GetFibonacciCallback(IntPtr numbers, int size);
 
         [DllImport(DllName, EntryPoint = nameof(CreateSampleFuncs))]
@@ -20,6 +24,30 @@ namespace SampleLib
         internal static extern void ReleaseSampleFuncs(SampleFuncsSafeHandle handle);
 
         [DllImport(DllName, EntryPoint = nameof(GetFibonacci))]
-        internal static extern void GetFibonacci(SampleFuncsSafeHandle handle, int n, GetFibonacciCallback callback);
+        static extern void GetFibonacci(SampleFuncsSafeHandle handle, int n, GetFibonacciCallback callback);
+
+        internal static Task<IEnumerable<int>> GetFibonacciAsync(SampleFuncsSafeHandle handle, int n)
+        {
+            var tcs = fibonacciCallbacks.GetOrAdd(n, new TaskCompletionSource<IEnumerable<int>>());
+            Task.Run(() => GetFibonacci(handle, n, SampleFuncsWrapper.GetFibonacciCallbackDelegate));
+
+            return tcs.Task;
+        }
+
+#if __IOS__
+        [ObjCRuntime.MonoPInvokeCallback(typeof(SampleFuncsWrapper.GetFibonacciCallback))]
+#endif
+        static void GetFibonacciCallbackDelegate(IntPtr numberPtr, int size)
+        {
+            TaskCompletionSource<IEnumerable<int>> tcs = null;
+
+            if (!fibonacciCallbacks.TryRemove(size, out tcs))
+                return;
+
+            int[] numbersArray = new int[size];
+            Marshal.Copy(numberPtr, numbersArray, 0, size);
+
+            tcs.SetResult(new List<int>(numbersArray));
+        }
     }
 }
