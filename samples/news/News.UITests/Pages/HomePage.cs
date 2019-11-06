39d7ba1567c7e0e4f40f458ae5bb7ff43ebd735d
Sweekriti Satpathy <swsat@microsoft.com>
2019-11-06 13:26:57 -0800

    fixing conflicts for master (#39)
    * [WIP] Readme fixes (#11)
    
    * Squashed Commits
    * ReadMe Updates
    * Sample Updates
    
    * Release pipeline updates
    
    Updated release badges & links to release pipelines
    
    * Updated cpp_with_xamarin sample to work with the latest Android NDK (#29)
    
    * Updated cpp_libs_xamarin read me addressing Xamarin.iOS limitations (#31)
    
    * readme update
    
    * readme update

---
+++ samples/news/News.UITests/Pages/HomePage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace News.UITests.Pages
+{
+    public class HomePage : BasePage
+    {
+        //Use the name of NewsPage since HomePage is not an actual Page
+        protected override PlatformQuery Trait => new PlatformQuery
+        {
+            Android = x => x.Marked(nameof(NewsPage)),
+            iOS = x => x.Marked(nameof(NewsPage))
+        };
+
+        protected readonly Query NewsTab;
+        protected readonly Query SourcesTab;
+        protected readonly Query FavoritesTab;
+        protected readonly Query SearchTab;
+        protected readonly Query SettingsTab;
+
+        public HomePage()
+        {
+            NewsTab = x => x.Marked("news");
+            SourcesTab = x => x.Marked("sources");
+            FavoritesTab = x => x.Marked("favorites");
+            SearchTab = x => x.Marked("search");
+            SettingsTab = x => x.Marked("settings");
+        }
+
+        public NewsPage ShowNewsTab()
+        {
+            app.WaitForElement(NewsTab);
+            app.Screenshot("Navigating to news tab");
+            app.Tap(NewsTab);
+            return new NewsPage();
+        }
+
+        public SourcesPage ShowSourcesTab()
+        {
+            app.WaitForElement(SourcesTab);
+            app.Screenshot("Navigating to sources tab");
+            app.Tap(SourcesTab);
+            return new SourcesPage();
+        }
+
+        public FavoritesPage ShowFavoritesTab()
+        {
+            app.WaitForElement(FavoritesTab);
+            app.Screenshot("Navigating to favorites tab");
+            app.Tap(FavoritesTab);
+            return new FavoritesPage();
+        }
+
+        public SearchPage ShowSearchTab()
+        {
+            app.WaitForElement(SearchTab);
+            app.Screenshot("Navigating to search tab");
+            app.Tap(SearchTab);
+            return new SearchPage();
+        }
+
+        public SettingsPage ShowSettingsTab()
+        {
+            app.WaitForElement(SettingsTab);
+            app.Screenshot("Navigating to settings tab");
+            app.Tap(SettingsTab);
+            return new SettingsPage();
+        }
+    }
+}
